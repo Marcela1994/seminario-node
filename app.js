3e3e5879db6dd145f9cd0f5aa631ca4e4963bf6e
mjgs <mgarcia@solusoft.es>
2015-06-23 15:20:14 +0200

    primer ejemplo

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+ Breve introducción a funciones y asíncronicidad en javascript
+ -------------------------------------------------------------
+ 
+ Forma rápida de ejecutar un programa en node. 
+ 
+*/
+
+[1,2,3,4,5].forEach(function(number) {
+	console.log('n ' + number);
+});
+
