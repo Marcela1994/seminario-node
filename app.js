cf283e4c8d2b601a153687e64af15e4628943398
mjgs <mgarcia@solusoft.es>
2015-06-23 17:01:08 +0200

    beautify

--- app.js
+++ app.js
@@ -2,11 +2,38 @@
  Breve introducción a funciones y asíncronicidad en javascript
  -------------------------------------------------------------
  
- Forma rápida de ejecutar un programa en node. 
+ El mismo ejemplo un poco mejor escrito 
  
 */
+
+/* código anterior
 
 [1,2,3,4,5].forEach(function(number) {
 	console.log('n ' + number);
 });
+
+*/
+
+/* código nuevo */
+
+var numbers = [1,2,3,4,5];
+
+// nuestro programa
+main();
+
+/**
+ * Programa principal
+ */
+function main(){
+	numbers.forEach(readFile);
+};
+
+/**
+ * Leemos el fichero correspondiente al numero especificado
+ * @param {Number} number
+ */
+function readFile(number){
+	console.log('n ' + number);
+};
+
 
