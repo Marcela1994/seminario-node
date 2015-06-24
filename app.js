a3a46b9717ea60521fc770218c6569df095844dd
mjgs <mgarcia@solusoft.es>
2015-06-24 17:43:10 +0200

    servidor sencillo en node

--- app.js
+++ app.js
@@ -6,3 +6,17 @@
  
 */
 
+var http = require('http');
+
+// Configuramos el servidor, que reponderá a todas las peticiones
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Bienvenido");
+  //response.end("Bienvenido a " + request.url);
+});
+
+// Escuchamos en el puerto 8000
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Servidor preparado en http://localhost:8000/");
