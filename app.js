f92296969da8cfa069ee8c4c02286d40ef9a98b4
mjgs <mgarcia@solusoft.es>
2015-06-24 17:27:20 +0200

    Reiniciando el proyecto, vamos a hacer WEBs

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
 /*
- Breve introducción a funciones y asíncronicidad en javascript
+ Breve introducción a node como servidor web
  -------------------------------------------------------------
  
- Ejemplo real: descargar paginas de resultado de busqueda de google
+ Hola Mundo en HTTP
  
 */
-
-var async 		= require('async');
-var generator	= require('./bll/generator');
-var downloader	= require('./bll/downloader');
-
-//fichero de configuracion
-var config  = require('./config.json');
-
-
-// nuestro programa
-main();
-
-/**
- * Programa principal
- */
-function main(){	
-	
-	var sites = generator.generateSites(config.query);
-	
-	async.each(
-		sites, 
-		downloader.downloadSingleUri,
-		processEndProgram
-	);
-};
-
-
-
-/**
- * Realizamos acciones una vez finalizada la ejecución del programa
- */
-function processEndProgram() {
-	console.log('Se descargaron todas las páginas');
-}
-
-
 
