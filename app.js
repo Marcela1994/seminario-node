172a01af15a45fa2eef9c0f81af279a7f4418bf7
mjgs <mgarcia@solusoft.es>
2015-06-25 13:39:43 +0200

    api, mock get y post

--- app.js
+++ app.js
@@ -6,15 +6,45 @@
  
 */
 
-var express = require('express');
-var app     = express();
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var app         = express();
 
-// Mapa de rutas
-app.get('/', function (req, res) {
-  res.send("Bienvenido a express");
+//configuramos la aplicacion 
+app.use(bodyParser.urlencoded({ extended: true }));   //nos permite obtener urlencode data desde body
+app.use(bodyParser.json());                           //nos permite obtener json data desde body
+
+// rutas de nuestro API
+// ==============================================
+
+var api = express.Router();  //creamos un router para el api o "mini-app"
+
+api.get('/messages', function (req, res) {
+  
+  var msg1 = { username: "findemor",  message: "soy el primero del chat!",  timestamp: new Date().toISOString() };
+  var msg2 = { username: "daniel",    message: "por poco tiempo...",        timestamp: new Date().toISOString() };
+  var messages = [ msg1, msg2 ];
+  
+  res.json(messages);
 });
 
+api.post('/messages', function (req, res) {
+  
+  var msg = req.body;
+      msg.timestamp = new Date().toISOString();
+  
+  //save msg
+  res.status(202);
+  res.send();
+});
+
+// cargamos el router
+
+app.use('/api', api);
+
 // Servidor
-var server = app.listen(8000, function() {
+// ==============================================
+var port = process.env.PORT || 8000;
+var server = app.listen(port, function() {
   console.log('Servidor preparado en http://localhost:%s', server.address().port);
 });
